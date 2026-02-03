2bfc9faeddebc16ee1dfafaea6a5dee057bd043e
Cristian <cchura.cpc@gmail.com>
2026-02-02 21:46:59 -0400

    migrationes con typeorm

---
+++ typeorm.config.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+import { DataSource } from "typeorm";
+
+config(); // carga las variables de entorno del archivo .env
+
+export default new DataSource({
+    type: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [__dirname + "/src/**/*.entity{.ts,.js}"],
+    migrations: [__dirname + "/src/database/migrations/*{.ts,.js}"],
+    synchronize: false
+});
